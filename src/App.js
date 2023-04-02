b285d9b40c3a154610cccf91e2572d027a6da1c4
moaaz90 <moaaz.yahia.zakaria@gmail.com>
2023-04-02 06:03:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-<button id="c1" ></button>
-<button id="c2" ></button>
-<button id="c3" ></button>
-<button id="c4" ></button>
-<button id="c5" ></button>
-<button id="c6" ></button>
-<button id="c7" ></button>
-<button id="c8" ></button>
-<button id="c9" ></button>
+      <h1 className="X-O"> XO Game
+</h1>
+      <div>
+<button id="c1" onclick="insert(this.id)"></button>
+<button id="c2" onclick="insert(this.id)"></button>
+<button id="c3" onclick="insert(this.id)"></button>
+  </div>
+  <div>
+<button id="c4" onclick="insert(this.id)"></button>
+<button id="c5" onclick="insert(this.id)"></button>
+<button id="c6" onclick="insert(this.id)"></button>
+</div>
+<div>
+<button id="c7" onclick="insert(this.id)"></button>
+<button id="c8" onclick="insert(this.id)"></button>
+<button id="c9" onclick="insert(this.id)"></button>
+</div>
     </div>
   );
 }
